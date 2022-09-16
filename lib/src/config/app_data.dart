3638fbc4069d5d78372a78db43e8de1cfb8a8f88
Adeilson Silva <adeilson.silva@brainweb.com.br>
2022-09-16 19:48:58 -0300

    section 8 - profile screen

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -1,5 +1,6 @@
 import 'package:greengrocer/src/models/cart_item_model.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 
 ItemModel apple = ItemModel(
   description:
@@ -87,3 +88,11 @@ List<CartItemModel> cartItems = [
     quantity: 3,
   ),
 ];
+
+UserModel user = UserModel(
+  name: 'Adeilson Silva',
+  email: 'adeilson@gmail.com',
+  phone: '55 19 99999-9999',
+  cpf: '999.999.999-99',
+  password: '',
+);
